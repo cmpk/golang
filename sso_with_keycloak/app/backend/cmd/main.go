d8ca37f5fadb0162602d8de0e7f9a06f6085098a
cmpk <chimtra6no@gmail.com>
2021-12-29 17:57:26 +0900

    Check token always

--- sso_with_keycloak/app/backend/cmd/main.go
+++ sso_with_keycloak/app/backend/cmd/main.go
@@ -6,6 +6,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"io"
 	"log"
 	"net/http"
@@ -22,6 +23,8 @@ var once sync.Once
 var provider *oidc.Provider
 var oauth2Config *oauth2.Config
 
+const OIDC_CALLBACK_PATH = "/api/set-token"
+
 func getConfig(req *http.Request) (*oauth2.Config, *oidc.Provider) {
 	once.Do(func() {
 		// See : https://github.com/coreos/go-oidc
@@ -35,72 +38,110 @@ func getConfig(req *http.Request) (*oauth2.Config, *oidc.Provider) {
 			ClientSecret: os.Getenv("CLIENT_SECRET"),
 			Endpoint:     provider.Endpoint(),
 			Scopes:       []string{oidc.ScopeOpenID},
-			RedirectURL:  os.Getenv("APP_URL") + "/api/callback",
+			RedirectURL:  os.Getenv("APP_URL") + OIDC_CALLBACK_PATH,
 		}
 	})
 	return oauth2Config, provider
 }
 
+func authenticate(w http.ResponseWriter, req *http.Request) {
+	config, _ := getConfig(req)
+	url := config.AuthCodeURL(os.Getenv("STATE_STRING"))
+
+	http.Redirect(w, req, url, http.StatusFound)
+}
+
+func checkToken(w http.ResponseWriter, req *http.Request, token string) (*oidc.IDToken, error) {
+	log.Printf("===== checkToken")
+	_, provider := getConfig(req)
+	if err := req.ParseForm(); err != nil {
+		return nil, errors.New("parse form error")
+	}
+	oidcConfig := &oidc.Config{
+		ClientID: os.Getenv("CLIENT_ID"),
+	}
+	verifier := provider.Verifier(oidcConfig)
+	idToken, err := verifier.Verify(context.Background(), token)
+	if err != nil {
+		log.Printf("id token verify error : " + err.Error())
+		return nil, err
+	}
+
+	return idToken, nil
+}
+
+func checkTokenHandler(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		rawIDTokenCookie, err := req.Cookie("token")
+		if err != nil {
+			authenticate(w, req) // Keycloak にリダイレクト
+			return
+		}
+
+		_, err = checkToken(w, req, rawIDTokenCookie.Value)
+		if err != nil {
+			authenticate(w, req) // Keycloak にリダイレクト
+			return
+		}
+
+		handler.ServeHTTP(w, req)
+	})
+}
+
+var SetTokenHandler = http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+	config, _ := getConfig(req)
+	if err := req.ParseForm(); err != nil {
+		http.Error(w, "parse form error", http.StatusInternalServerError)
+		return
+	}
+
+	accessToken, err := config.Exchange(context.Background(), req.Form.Get("code"))
+	if err != nil {
+		http.Error(w, "Can't get access token", http.StatusInternalServerError)
+		return
+	}
+
+	rawIDToken, ok := accessToken.Extra("id_token").(string)
+	if !ok {
+		http.Error(w, "missing token", http.StatusInternalServerError)
+		return
+	}
+
+	idToken, err := checkToken(w, req, rawIDToken)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// IDトークンのクレームをとりあえずダンプ
+	// アプリで必要なものはセッションストレージに入れておくと良いでしょう
+	idTokenClaims := map[string]interface{}{}
+	if err := idToken.Claims(&idTokenClaims); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	log.Printf("idTokenClaims = %#v", idTokenClaims)
+	http.SetCookie(w, &http.Cookie{
+		Name:  "token",
+		Value: rawIDToken, // 行儀が悪いので真似しないねで
+		Path:  "/api",
+	})
+	http.Redirect(w, req, "/api", http.StatusFound)
+})
+
+var TestHandler = http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "This is test.")
+})
+
 func main() {
 	r := http.NewServeMux()
 
 	// 認証で保護したいページ。ログインしていなければKeycloakのOpenID Connect認証ページに飛ばす
-	r.HandleFunc("/api", func(w http.ResponseWriter, req *http.Request) {
-		// クッキーがない時はリダイレクト
-		if _, err := req.Cookie("Authorization"); err != nil {
-			config, _ := getConfig(req)
-			url := config.AuthCodeURL(os.Getenv("STATE_STRING"))
-
-			http.Redirect(w, req, url, http.StatusFound)
-			return
-		}
-		io.WriteString(w, "login success")
-	})
+	r.Handle("/api", checkTokenHandler(TestHandler))
 
 	// OpenID Connectの認証が終わった時に呼ばれるハンドラ
 	// もろもろトークンを取り出したりした後に、クッキーを設定して元のページに飛ばす
-	r.HandleFunc("/api/callback", func(w http.ResponseWriter, req *http.Request) {
-		config, provider := getConfig(req)
-		if err := req.ParseForm(); err != nil {
-			http.Error(w, "parse form error", http.StatusInternalServerError)
-			return
-		}
-
-		accessToken, err := config.Exchange(context.Background(), req.Form.Get("code"))
-		if err != nil {
-			http.Error(w, "Can't get access token", http.StatusInternalServerError)
-			return
-		}
-
-		rawIDToken, ok := accessToken.Extra("id_token").(string)
-		if !ok {
-			http.Error(w, "missing token", http.StatusInternalServerError)
-			return
-		}
-		oidcConfig := &oidc.Config{
-			ClientID: os.Getenv("CLIENT_ID"),
-		}
-		verifier := provider.Verifier(oidcConfig)
-		idToken, err := verifier.Verify(context.Background(), rawIDToken)
-		if err != nil {
-			http.Error(w, "id token verify error : "+err.Error(), http.StatusInternalServerError)
-			return
-		}
-		// IDトークンのクレームをとりあえずダンプ
-		// アプリで必要なものはセッションストレージに入れておくと良いでしょう
-		idTokenClaims := map[string]interface{}{}
-		if err := idToken.Claims(&idTokenClaims); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		log.Printf("%#v", idTokenClaims)
-		http.SetCookie(w, &http.Cookie{
-			Name:  "Authorization",
-			Value: "Bearer " + rawIDToken, // 行儀が悪いので真似しないねで
-			Path:  "/api",
-		})
-		http.Redirect(w, req, "/api", http.StatusFound)
-	})
+	r.HandleFunc(OIDC_CALLBACK_PATH, SetTokenHandler)
 
 	// log.Println(http.ListenAndServe(":8080", r))  // リクエストをロギングしなくてもよい場合
 	log.Println(http.ListenAndServe(":8080", handlers.LoggingHandler(os.Stdout, r)))
